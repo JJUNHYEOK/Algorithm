51cc6c0c9d0f27821e268bfca325537ae6a4b010
JJUNHYEOK <spacehyuck@gmail.com>
2025-05-28 18:56:51 +0900

    update sorting code

--- Sorting/merge_sorting.c
+++ Sorting/merge_sorting.c
@@ -1,2 +1,48 @@
 // divide & conquer
+#include <stdio.h>
+#include <stdlib.h>
+#define SIZE 10
 
+int n;
+int sorted[SIZE];
+
+void merge(int list[], int left, int mid, int right){
+    int i, j, k, l;
+    i = left; j = mid+1; k = left;
+
+    while(i<=mid && j<=right){
+        if(list[i]<=list[j]){
+            sorted[k++] = list[i++];
+        }
+        else{
+            sorted[k++] = list[j++];
+        }
+
+    }
+
+    if(i>mid){
+        for(l=j; l<=right; l++){
+            sorted[k++] = list[l];
+        }
+    }
+
+    else{
+        for(l=i; l<=mid; l++){
+            sorted[k++] = list[l];
+        }
+    }
+
+    for(l=left; l<=right; l++){
+        list[l] = sorted[l];
+    }
+}
+
+void merge_sorting(int list[], int left, int right){
+    int mid;
+    if(left<right){
+        mid = (left+right)/2;
+        merge_sorting(list,left,mid);
+        merge_sorting(list, mid+1, right);
+        merge(list, left, mid, right);
+    }
+}
